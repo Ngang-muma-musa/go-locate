47a6bee13b677f39a807d52aa50f9c9fa8d05492
Ngang Muma <ngangmuma00@gmail.com>
2023-10-17 01:45:27 +0100

    added user model

--- model/user.go
+++ model/user.go
@@ -1,13 +1,52 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name"`
+	Username string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"-"`
 	IsAdmin  bool   `json:"-"`
 }
 
-//
+// Create User
+func CreateUser(user *User) error {
+	var existingUser User
+	newUser := db.Where("email = ?", user.Email).Limit(1).Find(&existingUser)
+	if newUser.RowsAffected > 0 {
+		return errors.New("Email already exist")
+	}
+	newUser = db.Create(user)
+	return newUser.Error
+}
+
+// Update user
+func Updateuser(user *User) error {
+	updatedUser := db.Save(user)
+	return updatedUser.Error
+}
+
+// Get user by Id
+func GetUserByID(ID uint) *User {
+	var user User
+	res := db.First(&user, ID)
+	if res.RowsAffected == 0 {
+		return nil
+	}
+	return &user
+}
+
+// Get user by email
+func GetUserByEmail(email string) *User {
+	var user User
+	res := db.First(&user, email)
+	if res.RowsAffected == 0 {
+		return nil
+	}
+	return &user
+}
