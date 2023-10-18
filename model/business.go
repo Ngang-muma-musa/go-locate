15e0217888c1abcdac02d1698313c18726402f4c
Ngang Muma <ngangmuma00@gmail.com>
2023-10-18 12:42:32 +0100

    added search business service

--- model/business.go
+++ model/business.go
@@ -9,13 +9,18 @@ import (
 
 type Business struct {
 	gorm.Model
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Location    string   `json:"loaction"`
-	Contact     []string `json:"contact"`
-	Category    []int    `json:"category"`
-	Verified    bool     `json:"verified"`
-	UserID      uint     `json:"userId"`
+	Name        string             `json:"name"`
+	Description string             `json:"description"`
+	Location    string             `json:"loaction"`
+	Contact     []string           `json:"contact"`
+	Category    []BusinessCategory `json:"category"`
+	Verified    bool               `json:"verified"`
+	UserID      uint               `json:"userId"`
+}
+
+type BusinessSearch struct {
+	Location string
+	Category int
 }
 
 // Create a new business
@@ -69,4 +74,18 @@ func GetBusinessByName(name string) *Business {
 		return nil
 	}
 	return &busines
+}
+
+// Find business by Location and Category
+
+func GetBusinessByCategoryOrBusiness(options BusinessSearch) ([]Business, error) {
+	var business []Business
+	res := db.Where("category = ?", options.Category).Find(&business)
+	if options.Location != "" {
+		res.Where("location = ?", options.Location)
+	}
+	if business == nil {
+		return nil, errors.New("Error gettin business")
+	}
+	return business, nil
 }
--- pkg/business.go
+++ pkg/business.go
@@ -5,12 +5,15 @@ import (
 	"go-locate/model"
 )
 
-func CreateBusiness(name string, description string, location string, contact []model.Contact, user model.User, category model.BusinessCategory) (*model.Business, error) {
+func CreateBusiness(name string, description string, location string, contact []string, user model.User, category []model.BusinessCategory) (*model.Business, error) {
 	business := &model.Business{
 		Name:        name,
 		Description: description,
 		Location:    location,
 		UserID:      user.ID,
+		Verified:    false,
+		Contact:     contact,
+		Category:    category,
 	}
 	if businessExist := model.GetBusinessByName(name); businessExist != nil {
 		return nil, errors.New("Business namae already taken")
